c1a525c917f8eb12f953911de17b6100ccff0fbd
Egor Kataev <off.d33d@gmail.com>
2023-12-12 16:47:21 +0600

    Переименование

---
+++ лабораторные работы/3 Кратность пары/Program.cs
@@ -0,0 +1,20 @@
+﻿class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите количество пар");
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < n; i++) 
+        {
+            Console.WriteLine("Введите первого члена пары");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второго члена пары");
+            int b = Convert.ToInt32(Console.ReadLine());
+            if(a>=b & a % 3 == 0) { Console.WriteLine(a); }
+            if(a>=b & a % 3 != 0 & b % 3 == 0) { Console.WriteLine(b); }
+            if(b>a & b % 3 == 0) { Console.WriteLine(b); }
+            if(b>a & b % 3 != 0 & a % 3 == 0) { Console.WriteLine(a); }
+
+        }
+    }
+}
