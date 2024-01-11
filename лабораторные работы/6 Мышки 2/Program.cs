dc0100c33df0fd46f22e328b62a6aca6438f1e44
Egor Kataev <off.d33d@gmail.com>
2024-01-11 22:24:10 +0600

    mish2

---
+++ лабораторные работы/6 Мышки 2/Program.cs
@@ -0,0 +1,42 @@
+﻿class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("Количество");
+        int colvo = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Белая мышь");
+        int wm = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("k");
+        int k = Convert.ToInt32(Console.ReadLine());
+        int[] mouses = new int[colvo];
+        for (int l = 0; l < colvo; l++) { mouses[l] = l; }
+        int str = 0;
+        while (Array.FindAll(mouses, element => element == -1).Length != mouses.Length - 1) {
+            mouses[Decider(mouses, str, k)] = -1;
+            str = Decider(mouses, str, k);
+        }
+        int num = Array.IndexOf(mouses, Array.Find(mouses, element => element != -1));
+        int c = 0;
+        Console.WriteLine(num);
+    }
+    static int Decider(int[] mouses, int n, int s)
+    {
+        int m = 0;
+        for (int i = 1, j = 0; j < s + 1; i++) {
+            if (n + i < mouses.Length) {
+                if (mouses[n + i] != -1){++j;}
+            }
+            else if (n + i > mouses.Length - 1){
+                int f = n + i;
+                while (f > mouses.Length - 1){f -= mouses.Length;}
+                if (mouses[f] != -1){++j;}
+            }
+            m = i;
+        }
+        n += m;
+        while (n > mouses.Length - 1){
+            n -= mouses.Length;
+        }
+        return n;
+    }
+}
