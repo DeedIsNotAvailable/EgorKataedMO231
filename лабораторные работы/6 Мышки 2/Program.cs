004abc8ee82aac8de5cd87a1aa99f202af441c31
Egor Kataev <off.d33d@gmail.com>
2024-01-12 08:19:11 +0600

    мышки2

--- лабораторные работы/6 Мышки 2/Program.cs
+++ лабораторные работы/6 Мышки 2/Program.cs
@@ -1,42 +1,76 @@
-﻿class HelloWorld
+﻿internal class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        Console.WriteLine("Количество");
-        int colvo = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Белая мышь");
-        int wm = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("k");
+
+        int c = Convert.ToInt32(Console.ReadLine());
+        int c1 = c;
+        int ost_c = Convert.ToInt32(Console.ReadLine());
+        int ost_c1 = ost_c;
+        int b = Convert.ToInt32(Console.ReadLine());
+        int b1 = b;
+        int ost_b = Convert.ToInt32(Console.ReadLine());
+        int ost_b1 = ost_b;
         int k = Convert.ToInt32(Console.ReadLine());
-        int[] mouses = new int[colvo];
-        for (int l = 0; l < colvo; l++) { mouses[l] = l; }
-        int str = 0;
-        while (Array.FindAll(mouses, element => element == -1).Length != mouses.Length - 1) {
-            mouses[Decider(mouses, str, k)] = -1;
-            str = Decider(mouses, str, k);
-        }
-        int num = Array.IndexOf(mouses, Array.Find(mouses, element => element != -1));
-        int c = 0;
-        Console.WriteLine(num);
-    }
-    static int Decider(int[] mouses, int n, int s)
-    {
-        int m = 0;
-        for (int i = 1, j = 0; j < s + 1; i++) {
-            if (n + i < mouses.Length) {
-                if (mouses[n + i] != -1){++j;}
+        int n = c + b;
+        int ost_n = (c - ost_c) + (b - ost_b);
+        int[] misi = new int[n];
+        int start_pos = 0;
+        for (int i = 0; i < ost_n; i++)
+        {
+            for (int j = 0; j < k;)
+            {
+                start_pos++;
+                while (start_pos > n - 1)
+                {
+                    start_pos = start_pos - n;
+                }
+                if (misi[start_pos] != -1 || misi[start_pos] != -2)
+                {
+                    j++;
+                }
             }
-            else if (n + i > mouses.Length - 1){
-                int f = n + i;
-                while (f > mouses.Length - 1){f -= mouses.Length;}
-                if (mouses[f] != -1){++j;}
+            if (ost_b1 != b)
+            {
+                misi[start_pos] = -1;
+                ost_b1++;
             }
-            m = i;
+            else if (ost_c1 != c)
+            {
+                misi[start_pos] = -2;
+                ost_c1++;
+            }
+
         }
-        n += m;
-        while (n > mouses.Length - 1){
-            n -= mouses.Length;
+        for (int i = 0; i < n; i++)
+        {
+            if (misi[i] == -1 || misi[i] == -2)
+            {
+                continue;
+            }
+            else if (c1 - (c - ost_c) != 0)
+            {
+                misi[i] = 2;
+                c1--;
+            }
+            else if (b1 - (b - ost_b) != 0)
+            {
+                misi[i] = 1;
+                b1--;
+            }
+
         }
-        return n;
+        Console.Write("Расположение мышей: ");
+        for (int i = 0; i < n; i++)
+        {
+            if (ost_b > b || ost_c > c)
+            {
+                Console.WriteLine("невозможно");
+                break;
+            }
+            if (misi[i] == 1 || misi[i] == -1) Console.Write("б ");
+            if (misi[i] == 2 || misi[i] == -2) Console.Write("с ");
+        }
+
     }
 }
