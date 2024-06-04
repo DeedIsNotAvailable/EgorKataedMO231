713d370df73d099f1fdd5a1e653e2ea4cf88c4d0
Egor Kataev <off.d33d@gmail.com>
2024-06-04 23:51:53 +0600

    ехх

---
+++ Семестр2/Алгоритмизация/задание про помытие машин/cw/Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+    delegate void Washing();
+    static void Main(string[] args)
+    {
+        Garage gr = new Garage();
+        gr.AddCar(new car("Авто 1"));
+        gr.AddCar(new car("Авто 2"));
+        gr.AddCar(new car("Авто 3"));
+        Washing washcar;
+        washcar = wash;
+        washcar();
+        void wash()
+        {
+            foreach (var car in Garage.cars)
+            {
+                Garage.carwash(car);
+            }
+        }
+    }
+}
+public class car
+{
+    public string name;
+    public car(string name)
+    {
+        this.name = name;
+    }
+}
+class Garage
+{
+    static public List<car> cars = new List<car>();
+    public void AddCar(car car)
+    {
+        cars.Add(car);
+    }
+    static public void carwash(car car)
+    {
+        Console.WriteLine($"Автомобиль {car.name} отмыт");
+    }
+}
