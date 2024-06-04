872e9b1a87971c0d0a783a420ae672864cb344c7
Egor Kataev <off.d33d@gmail.com>
2024-06-04 23:22:38 +0600

    мээ

---
+++ Семестр2/Олимпиадки/Золотая рыбка/gf/Program.cs
@@ -0,0 +1,109 @@
+﻿class gf
+{
+    static void Main(string[] args)
+    {
+        StreamReader StrR = new StreamReader("C:\\Users\\Deed\\Desktop\\inputs\\input.txt"); //input
+        StreamWriter StrW = new StreamWriter("output.txt");
+
+        string[] toito = StrR.ReadLine().Split();
+        int n = int.Parse(toito[0]);
+        int m = int.Parse(toito[1]);
+        string[] Coords = StrR.ReadLine().Split();
+        int xAxis = int.Parse(Coords[0]);
+        int yAxis = int.Parse(Coords[1]);
+        int zAxis = int.Parse(Coords[2]);
+        for (int i = 0; i < m; i++)
+        {
+            string[] axis = StrR.ReadLine().Split();
+            string ax = axis[0];
+            int ker = int.Parse(axis[1]);
+            int ser = int.Parse(axis[2]);
+            if (ax.Equals("X") && xAxis == ker)
+            {
+                List<List<int>> mas = new List<List<int>>();
+                for (int j = 0; j < n; j++)
+                {
+                    mas.Add(new List<int>());
+                    for (int l = 0; l < n; l++) mas[j].Add(0);
+                }
+                mas[yAxis - 1][zAxis - 1] = 1;
+                if (ser == 1) mas = spin(mas);
+                else mas = spin(spin(spin(mas)));
+                for (int j = 0; j < n; j++)
+                {
+                    for (int l = 0; l < n; l++)
+                    {
+                        if (mas[j][l] == 1)
+                        {
+                            yAxis = j + 1;
+                            zAxis = l + 1;
+                        }
+                    }
+                }
+            }
+            if (ax.Equals("Y") && yAxis == ker)
+            {
+                List<List<int>> mas = new List<List<int>>();
+                for (int j = 0; j < n; j++)
+                {
+                    mas.Add(new List<int>());
+                    for (int l = 0; l < n; l++) mas[j].Add(0);
+                }
+                mas[xAxis - 1][zAxis - 1] = 1;
+                if (ser == 1) mas = spin(mas);
+                else mas = spin(spin(spin(mas)));
+                for (int j = 0; j < n; j++)
+                {
+                    for (int l = 0; l < n; l++)
+                    {
+                        if (mas[j][l] == 1)
+                        {
+                            xAxis = j + 1;
+                            zAxis = l + 1;
+                        }
+                    }
+                }
+            }
+            if (ax.Equals("Z") && zAxis == ker)
+            {
+                List<List<int>> pole = new List<List<int>>();
+                for (int j = 0; j < n; j++)
+                {
+                    pole.Add(new List<int>());
+                    for (int l = 0; l < n; l++) pole[j].Add(0);
+                }
+
+                pole[xAxis - 1][yAxis - 1] = 1;
+
+                if (ser == 1) pole = spin(pole);
+
+                else pole = spin(spin(spin(pole)));
+                for (int j = 0; j < n; j++)
+                {
+                    for (int l = 0; l < n; l++)
+                    {
+                        if (pole[j][l] == 1)
+                        {
+                            xAxis = j + 1;
+                            yAxis = l + 1;
+                        }
+                    }
+                }
+            }
+        }
+        StrW.Write($"{xAxis} {yAxis} {zAxis}");
+        StrW.Close();
+    }
+
+    static List<List<int>> spin(List<List<int>> m)
+    {
+        List<List<int>> matdone = new List<List<int>>();
+        for (int i = 0; i < m[0].Count; i++)
+        {
+            List<int> res = new List<int>();
+            for (int j = m.Count - 1; j >= 0; j--) res.Add(m[j][i]);
+            matdone.Add(res);
+        }
+        return matdone;
+    }
+}
---
+++ Семестр2/Олимпиадки/Таукита/taukit/Program.cs
@@ -0,0 +1,34 @@
+﻿class taukit
+{
+    static void Main(string[] args)
+    {
+        StreamReader StreR = new StreamReader("C:\\Users\\Deed\\Desktop\\inputs\\input.txt");
+        StreamWriter StreaW = new StreamWriter("output.txt");
+        string[] tok = StreR.ReadLine().Split();
+        string[] ans = new string[tok.Length];
+        int rest = tok.Length;
+        rest = rest / 2;
+        int ta, tb, re;
+
+        for (int i = 0; i < tok.Length; i++)
+        {
+            if (i % 2 == 0) ta = i;
+            else ta = -i;
+            rest += ta;
+
+            re = tok[rest].Length / 2;
+
+            for (int j = 0; j < tok[rest].Length; j++)
+            {
+                if (j % 2 == 0) tb = j;
+                else tb = -j;
+                re += tb;
+
+                ans[i] += tok[rest][re];
+            }
+
+        }
+        StreaW.Write(string.Join(" ", ans));
+        StreaW.Close();
+    }
+}
