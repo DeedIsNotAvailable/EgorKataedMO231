9b231175c211ec7b07a4bcc38c68f5f10f79b460
Egor Kataev <off.d33d@gmail.com>
2024-06-05 00:34:15 +0600

    есть

---
+++ Семестр2/Дискретная Математика/Роберт Флорес/rofl/Program.cs
@@ -0,0 +1,39 @@
+﻿class fc
+{
+    static void Main(string[] args)
+    {
+        int[,] g = {
+            {0, 1, 1, 1, 0, 0, 0},
+            {1, 0, 1, 0, 1, 0, 1},
+            {1, 1, 0, 1, 1, 1, 1},
+            {1, 0, 1, 0, 1, 1, 1},
+            {0, 1, 1, 1, 0, 1, 0},
+            {0, 0, 1, 1, 1, 0, 1},
+            {0, 1, 1, 1, 0, 1, 0},
+        };
+        int V = g.GetLength(0);
+        List<int> points = new List<int>();
+        rf(0, 0);
+        void rf(int v, int count)
+        {
+            points.Add(v);
+            if (count == V - 1)
+            {
+                if (g[v, 0] == 1)
+                {
+                    Console.WriteLine("Гамильтонов цикл:");
+                    foreach (int vr in points) Console.Write(vr + 1 + " ");
+                    return;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < V; i++)
+                {
+                    if (g[v, i] == 1 && !points.Contains(i)) rf(i, count + 1);
+                }
+            }
+            points.RemoveAt(points.Count - 1);
+        }
+    }
+}
---
+++ Семестр2/Дискретная Математика/Форд Фалкерсон/fordfalk/Program.cs
@@ -0,0 +1,72 @@
+﻿class Program
+{
+    static List<List<int>> graph = new List<List<int>>();
+    static List<Edge> edges = new List<Edge>();
+    static int[] alreadyUsed = new int[5];
+    static int t = 1;
+    static int s, f, ans;
+
+    static void Main(string[] args)
+    {
+        List<int[]> intiate = new List<int[]>
+        {
+            new int[] {0, 1, 20}, new int[] {0, 2, 30}, new int[] {0, 3, 10},
+            new int[] {1, 2, 40}, new int[] {1, 4, 30}, new int[] {2, 3, 10},
+            new int[] {2, 4, 20}, new int[] {3, 4, 20}
+        };
+
+        s = 0;
+        f = 4;
+        ans = 0;
+
+        for (int i = 0; i < 5; i++) graph.Add(new List<int>());
+        foreach (var a in intiate) addEdgee(a[0], a[1], a[2]);
+        while (FF(s, int.MaxValue) > 0) t++;
+        foreach (var a in graph[s]) ans += edges[a].flow;
+        Console.WriteLine(ans);
+    }
+    static void addEdgee(int vO, int vN, int weight)
+    {
+        graph[vO].Add(edges.Count);
+        edges.Add(new Edge(vN, weight));
+        graph[vN].Add(edges.Count);
+        edges.Add(new Edge(vO, 0));
+    }
+    static int FF(int versh, int minCapacity)
+    {
+        if (versh == f) return minCapacity;
+        alreadyUsed[versh] = t;
+
+        foreach (var i in graph[versh])
+        {
+            if (edges[i].gvc() == 0 || alreadyUsed[edges[i].point] == t) continue;
+            int x = FF(edges[i].point, Math.Min(minCapacity, edges[i].gvc()));
+            if (x > 0)
+            {
+                edges[i].flow += x;
+                edges[i ^ 1].flow -= x;
+                return x;
+            }
+        }
+        return 0;
+    }
+}
+
+class Edge
+{
+    public int point;
+    public int flow;
+    public int weight;
+
+    public Edge(int versh, int weight)
+    {
+        this.point = versh;
+        this.flow = 0;
+        this.weight = weight;
+    }
+
+    public int gvc()
+    {
+        return this.weight - this.flow;
+    }
+}
